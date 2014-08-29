795e094e21c3d80f64f38bdfb675797f55276709
Chap Lovejoy <chaplovejoy@gmail.com>
2014-08-29 13:22:20 -0400

    Add ability to specify JSX options

--- include/erlastic_search.hrl
+++ include/erlastic_search.hrl
@@ -6,7 +6,8 @@
           port        = 9200 :: integer(),
 
           % These are passed verbatim to the underlying http client in use.
-          http_client_options = []:: [term()], 
+          http_client_options = []:: [term()],
+          jsx_decode_options = [] :: [term()],
 
           % Keeping the following two options for backwards compatibility.
           timeout     = infinity :: integer() | infinity,
